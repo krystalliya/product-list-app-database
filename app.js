47d879cadba9324bc20338e5d1575108ae0c10a1
krystalliya <krystalliya@gmail.com>
2022-07-12 16:24:24 +0800

    added defaultProduct array, tested endpoints with postman

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model("Product", productSchema);
 
-const product = new Product({
+const product1 = new Product({
   key: 1,
   name: "Fishtail Mix 200pc",
   costInRMB: 6.5,
@@ -32,7 +32,31 @@ const product = new Product({
     "The purpose of lorem ipsum is to create a natural looking block of text.",
 });
 
-// product.save();
+const product2 = new Product({
+  key: 2,
+  name: "A-Fairy Mix 200pc",
+  costInRMB: 5.5,
+  usualPrice: 5.5,
+  description:
+    "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+});
+
+const product3 = new Product({
+  key: 3,
+  name: "Lower Lash 120pc",
+  costInRMB: 3.5,
+  usualPrice: 4.5,
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+});
+
+const defaultProducts = [product1, product2, product3];
+
+// defaultProducts.save();
+
+app.post("/test", function (req, res) {
+  res.send("done!");
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
